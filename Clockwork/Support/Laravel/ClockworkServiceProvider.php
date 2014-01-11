c8916beee56894b6e760f6eac09dfb36e0d1692a
itsgoingd <itsgoingd@luzer.sk>
2014-01-11 16:47:55 +0100

    Laravel - refactored the service provider.

--- Clockwork/Support/Laravel/ClockworkServiceProvider.php
+++ Clockwork/Support/Laravel/ClockworkServiceProvider.php
@@ -13,44 +13,45 @@ class ClockworkServiceProvider extends ServiceProvider
 {
 	public function boot()
 	{
+		$this->package('itsgoingd/clockwork', 'clockwork', __DIR__);
+
+		if (!$this->isCollectingData()) {
+			return; // Don't bother registering event listeners as we are not collecting data
+		}
+
+		$this->app['clockwork.laravel']->listenToEvents();
+
+		if (!$this->isEnabled()) {
+			return; // Clockwork is diabled, don't register the route
+		}
+
+		$app = $this->app;
+		$this->app['router']->get('/__clockwork/{id}', function($id = null, $last = null) use($app)
+		{
+			return $app['clockwork']->getStorage()->retrieveAsJson($id, $last);
+		})->where('id', '[0-9\.]+');
 	}
 
 	public function register()
 	{
-		$this->app['config']->package('itsgoingd/clockwork', __DIR__ . '/config');
-
-		$this->registerCommands();
-
-		$isEnabled = $this->app['config']->get('clockwork::enable');
-		if ($isEnabled === null) {
-			$isEnabled = $this->app['config']->get('app.debug');
-		}
-
-		$isCollectingData = $isEnabled || $this->app['config']->get('clockwork::collect_data_always', true);
-
-		if (!$isCollectingData) {
-			return; // Don't bother creating all the objects when we are not collecting data
-		}
-
-		$this->app['clockwork.laravel'] = $this->app->share(function($app){
-			$datasource = new LaravelDataSource($app);
-
-			return $datasource;
+		$this->app->singleton('clockwork.laravel', function($app)
+		{
+			return new LaravelDataSource($app);
 		});
 
-		$this->app['clockwork.laravel']->listenToEvents();
-
-		$this->app['clockwork'] = $this->app->share(function($app){
+		$this->app->singleton('clockwork', function($app)
+		{
 			$clockwork = new Clockwork();
 
 			$clockwork
 				->addDataSource(new PhpDataSource())
 				->addDataSource($app['clockwork.laravel']);
 
-			$filter = $app['config']->get('clockwork::filter', array());
+			$filter = $app['config']->get('clockwork::config.filter', array());
 
-			if ($app['config']->get('database.default') && !in_array('databaseQueries', $filter))
+			if ($app['config']->get('database.default') && !in_array('databaseQueries', $filter)) {
 				$clockwork->addDataSource(new EloquentDataSource($app['db']->connection()));
+			}
 
 			$storage = new FileStorage($app['path.storage'] . '/clockwork');
 			$storage->filter = $filter;
@@ -60,12 +61,19 @@ class ClockworkServiceProvider extends ServiceProvider
 			return $clockwork;
 		});
 
-		$app = $this->app;
-		$this->app->after(function($request, $response) use($app, $isEnabled){
-			// don't collect data for configured URIs
+		$this->registerCommands();
 
+		$app = $this->app;
+		$service = $this;
+		$this->app->after(function($request, $response) use($app, $service)
+		{
+			if (!$service->isCollectingData()) {
+				return; // Collecting data is disabled, return immediately
+			}
+
+			// don't collect data for configured URIs
 			$request_uri = $app['request']->getRequestUri();
-			$filter_uris = $app['config']->get('clockwork::filter_uris', array());
+			$filter_uris = $app['config']->get('clockwork::config.filter_uris', array());
 			$filter_uris[] = '/__clockwork/[0-9\.]+'; // don't collect data for Clockwork requests
 
 			foreach ($filter_uris as $uri) {
@@ -81,30 +89,24 @@ class ClockworkServiceProvider extends ServiceProvider
 			$app['clockwork']->resolveRequest();
 			$app['clockwork']->storeRequest();
 
-			if ($isEnabled) {
-				$response->headers->set('X-Clockwork-Id', $app['clockwork']->getRequest()->id, true);
-				$response->headers->set('X-Clockwork-Version', Clockwork::VERSION, true);
+			if (!$service->isEnabled()) {
+				return; // Clockwork is disabled, don't set the headers
+			}
 
-				if ($app['request']->getBasePath()) {
-					$response->headers->set('X-Clockwork-Path', $app['request']->getBasePath() . '/__clockwork/', true);
-				}
-				
-				$extraHeaders = $app['config']->get('clockwork::headers');
-				if ($extraHeaders and is_array($extraHeaders)) {
-					foreach ($extraHeaders as $headerName => $headerValue) {
-						$response->headers->set('X-Clockwork-Header-'.$headerName, $headerValue);
-					}
+			$response->headers->set('X-Clockwork-Id', $app['clockwork']->getRequest()->id, true);
+			$response->headers->set('X-Clockwork-Version', Clockwork::VERSION, true);
+
+			if ($app['request']->getBasePath()) {
+				$response->headers->set('X-Clockwork-Path', $app['request']->getBasePath() . '/__clockwork/', true);
+			}
+			
+			$extra_headers = $app['config']->get('clockwork::config.headers');
+			if ($extra_headers && is_array($extra_headers)) {
+				foreach ($extra_headers as $header_name => $header_value) {
+					$response->headers->set('X-Clockwork-Header-' . $header_name, $header_value);
 				}
 			}
 		});
-
-		if (!$isEnabled) {
-			return; // Don't bother registering the route when we are sending 404 anyway
-		}
-
-		$this->app['router']->get('/__clockwork/{id}', function($id = null, $last = null) use($app){
-			return $app['clockwork']->getStorage()->retrieveAsJson($id, $last);
-		})->where('id', '[0-9\.]+');
 	}
 
 	/**
@@ -125,5 +127,21 @@ class ClockworkServiceProvider extends ServiceProvider
 	public function provides()
 	{
 		return array('clockwork');
+	}
+
+	private function isEnabled()
+	{
+		$is_enabled = $this->app['config']->get('clockwork::config.enable', null);
+
+		if ($is_enabled === null) {
+			$is_enabled = $this->app['config']->get('app.debug');
+		}
+
+		return $is_enabled;
+	}
+
+	private function isCollectingData()
+	{
+		return $this->isEnabled() || $this->app['config']->get('clockwork::config.collect_data_always', true);
 	}
 }
