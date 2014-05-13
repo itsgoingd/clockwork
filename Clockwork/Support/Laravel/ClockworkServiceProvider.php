9db3184a2672ab9dc72db669095fe5c5841c5888
itsgoingd <itsgoingd@luzer.sk>
2014-05-14 00:23:21 +0200

    Revert "Merge pull request #54 from ptrofimov/log-all-queries"
    This reverts commit 9b6ac309839ccec01e537baa16901c692e56ff9d, reversing
    changes made to 4824b3bc1c17fcccf5e9df196a2ae25f5d111e16.

--- Clockwork/Support/Laravel/ClockworkServiceProvider.php
+++ Clockwork/Support/Laravel/ClockworkServiceProvider.php
@@ -75,7 +75,7 @@ class ClockworkServiceProvider extends ServiceProvider
 
 		$app = $this->app;
 		$service = $this;
-		$this->app->finish(function($request, $response) use($app, $service)
+		$this->app->after(function($request, $response) use($app, $service)
 		{
 			if (!$service->isCollectingData()) {
 				return; // Collecting data is disabled, return immediately
