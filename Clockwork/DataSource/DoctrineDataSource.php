f33f09bfbf141eed63ce9d5222bbf41c296c8c63
its <itsgoingd@luzer.sk>
2016-02-23 16:52:07 +0100

    Doctrine data source code style fixes.

--- Clockwork/DataSource/DoctrineDataSource.php
+++ Clockwork/DataSource/DoctrineDataSource.php
@@ -8,170 +8,148 @@ use Doctrine\ORM\EntityManager;
 
 class DoctrineDataSource extends DataSource implements SQLLogger
 {
-    /**
-     * Internal array where queries are stored
-     * @var array
-     */
-    protected $queries = array();
+	/**
+	 * Internal array where queries are stored
+	 */
+	protected $queries = array();
 
-    /**
-     * Doctrine event manager
-     *
-     * @var EventManager
-     */
-    protected $evm;
+	/**
+	 * Doctrine entity manager
+	 */
+	protected $enm;
 
-    /**
-     * Doctrine entity manager
-     *
-     * @var EntityManager
-     */
-    protected $enm;
+	/**
+	 * For timing queries:
+	 */
+	public $start = null;
 
-    /**
-     * @var Logger
-     */
-    protected $logger;
+	/**
+	 * Current recorded query
+	 */
+	public $query = null;
 
-    /**
-     * For timing queries:
-     * @var float|null
-     */
-    public $start = null;
+	/**
+	 * Doctrine connection
+	 *
+	 * @var Connection
+	 */
+	protected $connection;
 
-    /**
-     * @var null
-     */
-    public $query = null;
+	public function __construct(EntityManager $enm)
+	{
+		$this->enm        = $enm;
+		$this->connection = $enm->getConnection();
 
-    /**
-     * Doctrine connection
-     *
-     * @var Connection
-     */
-    protected $connection;
+		$enm->getConnection()->getConfiguration()->setSQLLogger($this);
+	}
 
-    public function __construct(EntityManager $enm)
-    {
-        $this->evm        = $enm->getEventManager(); //Unused, for now
-        $this->enm        = $enm;
-        $this->connection = $enm->getConnection();
+	/**
+	 * From SQLLogger Doctrine Interface
+	 *
+	 * @param string $sql
+	 * @param array $params
+	 * @param array $types
+	 */
+	public function startQuery($sql, array $params = null, array $types = null)
+	{
+		$this->start = microtime(true);
+		$sql         = $this->replaceParams($sql, $params);
+		$sql         = $this->formatQuery($sql);
 
-        $enm->getConnection()->getConfiguration()->setSQLLogger($this);
-    }
+		$this->query = array('sql' => $sql, 'params' => $params, 'types' => $types);
+	}
 
-    /**
-     * From SQLLogger Doctrine Interface
-     *
-     * @param string $sql
-     * @param array $params
-     * @param array $types
-     */
-    public function startQuery($sql, array $params = null, array $types = null)
-    {
-        $this->start    = microtime(true);
-        $sql            = $this->replaceParams($sql, $params);
-        $sql            = $this->formatQuery($sql);
+	protected function formatQuery($sql)
+	{
+		$keywords = array('select', 'insert', 'update', 'delete', 'where', 'from', 'limit', 'is', 'null', 'having', 'group by', 'order by', 'asc', 'desc');
+		$regexp   = '/\b' . implode('\b|\b', $keywords) . '\b/i';
 
-        $this->query    = array( 'sql' => $sql, 'params' => $params, 'types' => $types );
-    }
+		$sql = preg_replace_callback($regexp, function($match){
+			return strtoupper($match[0]);
+		}, $sql);
 
-    protected function formatQuery($sql)
-    {
-        $keywords   = array('select', 'insert', 'update', 'delete', 'where', 'from', 'limit', 'is', 'null', 'having', 'group by', 'order by', 'asc', 'desc');
-        $regexp     = '/\b' . implode('\b|\b', $keywords) . '\b/i';
+		return $sql;
+	}
 
-        $sql = preg_replace_callback($regexp, function($match){
-            return strtoupper($match[0]);
-        }, $sql);
+	protected function replaceParams($sql, $params)
+	{
+		if (is_array($params)) {
+			$pattern = '/\?/';
 
-        return $sql;
-    }
+			foreach ($params as $param) {
+				$param = $this->convertParam($param);
+				$sql   = preg_replace($pattern, "\"$param\"", $sql, 1);
+			}
+		}
 
-    protected function replaceParams($sql, $params)
-    {
-        if (is_array($params)) {
+		return $sql;
+	}
 
-            $pattern        = '/\?/';
+	protected function convertParam($param)
+	{
+		if (is_object($param)) {
+			if (! method_exists($param, '__toString')) {
+				// Carbon Object
+				if (is_a($param, 'DateTime')) {
+					$param = $param->format('Y-m-d');
+				} else {
+					throw new \Exception('Unstringable Object');
+				}
+			}
+		}
 
-            foreach ($params as $param) {
+		return $param;
+	}
 
-                $param  = $this->convertParam($param);
-                $sql    = preg_replace($pattern, "\"$param\"", $sql, 1);
-            }
-        }
+	/**
+	 * From SQLLogger Doctrine Interface
+	 */
+	public function stopQuery()
+	{
+		$endTime = microtime(true) - $this->start;
 
-        return $sql;
-    }
+		$this->registerQuery($this->query['sql'], $this->query['params'], $endTime, $this->connection->getDatabase());
+	}
 
-    protected function convertParam($param)
-    {
-        if (is_object($param)) {
+	/**
+	 * Log the query into the internal store
+	 * @return array
+	 */
+	public function registerQuery($query, $bindings, $time, $connection)
+	{
+		$this->queries[] = array(
+			'query'      => $query,
+			'bindings'   => $bindings,
+			'time'       => $time,
+			'connection' => $connection
+		);
+	}
 
-            if (!method_exists($param, '__toString')) {
+	/**
+	 * Adds ran database queries to the request
+	 */
+	public function resolve(Request $request)
+	{
+		$request->databaseQueries = array_merge($request->databaseQueries, $this->getDatabaseQueries());
 
-                // Carbon Object
-                if (is_a($param, 'DateTime'))
-                    $param = $param->format('Y-m-d');
-                else
-                    throw new \Exception('Unstringable Object');
-            }
-        }
+		return $request;
+	}
 
-        return $param;
-    }
+	/**
+	 * Returns an array of runnable queries and their durations from the internal array
+	 */
+	protected function getDatabaseQueries()
+	{
+		$queries = array();
+		
+		foreach ($this->queries as $query) {
+			$queries[] = array(
+				'query'      => $query['query'],
+				'duration'   => $query['time'],
+				'connection' => $query['connection']
+			);
+		}
 
-    /**
-     * From SQLLogger Doctrine Interface
-     */
-    public function stopQuery()
-    {
-        $endTime = microtime(true) - $this->start;
-
-        /**
-         * TODO Improve Connection name
-         * For now, $this->connection->getDatabase() return the name of the Database
-         */
-
-        $this->registerQuery($this->query['sql'], $this->query['params'], $endTime, $this->connection->getDatabase());
-    }
-
-    /**
-     * Log the query into the internal store
-     * @return array
-     */
-    public function registerQuery($query, $bindings, $time, $connection)
-    {
-        $this->queries[] = array(
-            'query'      => $query,
-            'bindings'   => $bindings,
-            'time'       => $time,
-            'connection' => $connection
-        );
-    }
-
-    /**
-     * Adds ran database queries to the request
-     */
-    public function resolve(Request $request)
-    {
-        $request->databaseQueries = array_merge($request->databaseQueries, $this->getDatabaseQueries());
-        return $request;
-    }
-
-    /**
-     * Returns an array of runnable queries and their durations from the internal array
-     */
-    protected function getDatabaseQueries()
-    {
-        $queries = array();
-        foreach ($this->queries as $query)
-            $queries[] = array(
-                'query'      => $query['query'],
-                'duration'   => $query['time'],
-                'connection' => $query['connection']
-            );
-
-        return $queries;
-    }
+		return $queries;
+	}
 }
