15735bcb7c63c5afe9bf81e5c1099bf34784c1a9
its <itsgoingd@luzer.sk>
2016-05-29 15:19:43 +0200

    Fixed Clockwork compatibility with Laravel 4.0.

--- Clockwork/Support/Laravel/Controllers/OldController.php
+++ Clockwork/Support/Laravel/Controllers/OldController.php
@@ -1,14 +1,13 @@
 <?php namespace Clockwork\Support\Laravel\Controllers;
 
-use Illuminate\Foundation\Application;
 use Illuminate\Routing\Controllers\Controller;
 
 class OldController extends Controller {
 
 	public $app;
 
-	public function __construct(Application $app) {
-		$this->app = $app;
+	public function __construct() {
+		$this->app = app();
 	}
 
 	public function getData($id = null, $last = null) {
