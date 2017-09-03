351caa171b499ec37834c23aebf263366f2ef4f3
Jergus Lejko <jergus.lejko@gmail.com>
2017-09-03 17:57:11 +0200

    refactor clock helper

--- Clockwork/Support/Lumen/helpers.php
+++ Clockwork/Support/Lumen/helpers.php
@@ -1,22 +1,19 @@
 <?php
 
 if (! function_exists('clock')) {
-	// workaround so we can log null values with the helepr function
-	if (! defined('CLOCKWORK_NULL')) {
-		define('CLOCKWORK_NULL', sha1(time()));
-	}
-
 	/**
 	 * Log a message to Clockwork, returns Clockwork instance when called with no arguments.
 	 */
-	function clock($message = CLOCKWORK_NULL)
+	function clock()
 	{
-		if ($message === CLOCKWORK_NULL) {
+		$arguments = func_get_args();
+
+		if (empty($arguments)) {
 			return app('clockwork');
-		} else {
-			foreach (func_get_args() as $arg) {
-				app('clockwork')->debug($arg);
-			}
+		}
+
+		foreach ($arguments as $argument) {
+			app('clockwork')->debug($argument);
 		}
 	}
 }
