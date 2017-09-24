44705a5afcf3ce0c91860c88f53a47a18e7d3b2d
its <itsgoingd@luzer.sk>
2017-09-24 23:59:55 +0200

    PSR message data source now returns URIs consistent with other data sources, code style fixes.

--- Clockwork/DataSource/DoctrineDataSource.php
+++ Clockwork/DataSource/DoctrineDataSource.php
@@ -4,7 +4,6 @@ use Doctrine\ORM\EntityManager;
 
 class DoctrineDataSource extends DBALDataSource
 {
-
 	public function __construct(EntityManager $enm)
 	{
 		parent::__construct($enm->getConnection());
