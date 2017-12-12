3ce53814539eca23a508ccb8c699c1b047e6f088
its <itsgoingd@luzer.sk>
2017-12-12 18:44:11 +0100

    Dropped Laravel 4 support in EloquentDataSource.

--- Clockwork/DataSource/EloquentDataSource.php
+++ Clockwork/DataSource/EloquentDataSource.php
@@ -4,7 +4,6 @@ use Clockwork\Helpers\StackTrace;
 use Clockwork\Request\Request;
 use Clockwork\Support\Laravel\Eloquent\ResolveModelScope;
 use Clockwork\Support\Laravel\Eloquent\ResolveModelLegacyScope;
-use Clockwork\Support\Laravel\Eloquent\ResolveModelOldScope;
 
 use Illuminate\Database\DatabaseManager;
 use Illuminate\Contracts\Events\Dispatcher as EventDispatcher;
@@ -54,10 +53,10 @@ class EloquentDataSource extends DataSource
 		}
 
 		if (class_exists('Illuminate\Database\Events\QueryExecuted')) {
-			// Laravel 5.2
+			// Laravel 5.2 and up
 			$this->eventDispatcher->listen('Illuminate\Database\Events\QueryExecuted', [ $this, 'registerQuery' ]);
 		} else {
-			// Laravel 4.0 to 5.1
+			// Laravel 5.0 to 5.1
 			$this->eventDispatcher->listen('illuminate.query', [ $this, 'registerLegacyQuery' ]);
 		}
 	}
@@ -171,17 +170,11 @@ class EloquentDataSource extends DataSource
 	 */
 	protected function getModelResolvingScope()
 	{
-		if (interface_exists('Illuminate\Database\Eloquent\Scope')) {
-			// Laravel 5.2
-			return new ResolveModelScope($this);
-		} elseif (interface_exists('Illuminate\Database\Eloquent\ScopeInterface')) {
-			if (trait_exists('Illuminate\Database\Eloquent\SoftDeletingTrait')) {
-				// Laravel 4.2
-				return new ResolveModelOldScope($this);
-			} else {
-				// Laravel 5.0 to 5.1
-				return new ResolveModelLegacyScope($this);
-			}
+		if (interface_exists('Illuminate\Database\Eloquent\ScopeInterface')) {
+			// Laravel 5.0 to 5.1
+			return new ResolveModelLegacyScope($this);
 		}
+
+		return new ResolveModelScope($this);
 	}
 }
