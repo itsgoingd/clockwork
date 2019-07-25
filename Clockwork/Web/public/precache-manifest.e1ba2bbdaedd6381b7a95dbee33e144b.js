9dd27056045d3961ff0acb2655f8e233905fb97f
its <itsgoingd@luzer.sk>
2019-07-26 00:48:50 +0200

    Updated Clockwork App to 4.0.3.

---
+++ Clockwork/Web/public/precache-manifest.e1ba2bbdaedd6381b7a95dbee33e144b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c9f260009344314aa60",
+    "url": "css/app.c0baaa16.css"
+  },
+  {
+    "revision": "3c9f260009344314aa60",
+    "url": "js/app.92cf2a3f.js"
+  },
+  {
+    "revision": "d70b6adec04e39b28c68",
+    "url": "js/chunk-vendors.de559e1a.js"
+  },
+  {
+    "revision": "8e0ffd488e8b716b76ab32e6233cac9a",
+    "url": "index.html"
+  }
+];
--- Clockwork/Web/public/service-worker.js
+++ Clockwork/Web/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7e830f2c8b9d20bb86300eb583a90aec.js"
+  "precache-manifest.e1ba2bbdaedd6381b7a95dbee33e144b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "Clockwork"});
