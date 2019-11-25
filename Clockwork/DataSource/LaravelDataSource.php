711f26aa314efcafefe855402124bd5ca28a41d9
its <itsgoingd@luzer.sk>
2019-11-25 22:05:54 +0100

    Fixed compatibility with Laravel 5.4 and earlier.

--- Clockwork/DataSource/LaravelDataSource.php
+++ Clockwork/DataSource/LaravelDataSource.php
@@ -279,7 +279,7 @@ class LaravelDataSource extends DataSource
 	// Add authenticated user data to the request
 	protected function resolveAuthenticatedUser(Request $request)
 	{
-		if (! $this->app->has('auth')) return;
+		if (! $this->app->bound('auth')) return;
 		if (! ($user = $this->app['auth']->user())) return;
 		if (! isset($user->email) || ! isset($user->id)) return;
 
