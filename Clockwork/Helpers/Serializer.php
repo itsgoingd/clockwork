49abe2886163a9c60fbe8745623a92576744af12
its <its@luzer.sk>
2020-01-20 01:11:31 +0100

    Cleaned up serializer implementationa a bit, fixed serialized objects not being cached when casted to string.

--- Clockwork/Helpers/Serializer.php
+++ Clockwork/Helpers/Serializer.php
@@ -39,36 +39,28 @@ class Serializer
 		if ($context === null) $context = [ 'references' => [] ];
 		if ($limit === null) $limit = $this->options['limit'];
 
-		if ($data instanceof \Closure) {
-			return [ '__type__' => 'anonymous function' ];
-		} elseif (is_array($data)) {
+		if (is_array($data)) {
 			if ($limit === 0) return [ '__type__' => 'array', '__omitted__' => 'limit' ];
 
-			return [ '__type__' => 'array' ] + array_map(function ($item) use ($context, $limit) {
-				return $this->normalize($item, $context, $limit - 1);
-			}, $data);
+			return [ '__type__' => 'array' ] + $this->normalizeEach($data, $context, $limit - 1);
 		} elseif (is_object($data)) {
+			if ($data instanceof \Closure) return [ '__type__' => 'anonymous function' ];
+
 			$className = get_class($data);
 			$objectHash = spl_object_hash($data);
 
 			if ($limit === 0) return [ '__class__' => $className, '__omitted__' => 'limit' ];
 
-			if ($this->options['toString'] && method_exists($data, '__toString')) {
-				return (string) $data;
-			}
-
-			if (isset($context['references'][$objectHash])) {
-				return [ '__type__' => 'recursion' ];
-			}
+			if (isset($context['references'][$objectHash])) return [ '__type__' => 'recursion' ];
 
 			$context['references'][$objectHash] = true;
 
-			if (isset($this->cache[$objectHash])) {
-				return $this->cache[$objectHash];
-			}
+			if (isset($this->cache[$objectHash])) return $this->cache[$objectHash];
 
 			if ($this->options['blackbox'] && in_array($className, $this->options['blackbox'])) {
 				return $this->cache[$objectHash] = [ '__class__' => $className, '__omitted__' => 'blackbox' ];
+			} elseif ($this->options['toString'] && method_exists($data, '__toString')) {
+				return $this->cache[$objectHash] = (string) $data;
 			}
 
 			if ($this->options['debugInfo'] && method_exists($data, '__debugInfo')) {
@@ -81,14 +73,14 @@ class Serializer
 				$data = (array) $data;
 			}
 
-			$data = array_column(array_map(function ($key, $item) use ($className, $context, $limit) {
-				return [
+			$data = array_combine(
+				array_map(function ($key) {
 					// replace null-byte prefixes of protected and private properties used by php with * (protected)
 					// and ~ (private)
-					preg_replace('/^\0.+?\0/', '~', str_replace("\0*\0", '*', $key)),
-					$this->normalize($item, $context, $limit - 1)
-				];
-			}, array_keys($data), $data), 1, 0);
+					return preg_replace('/^\0.+?\0/', '~', str_replace("\0*\0", '*', $key));
+				}, array_keys($data)),
+				$this->normalizeEach($data, $context, $limit - 1)
+			);
 
 			return $this->cache[$objectHash] = [ '__class__' => $className ] + $data;
 		} elseif (is_resource($data)) {
@@ -99,8 +91,10 @@ class Serializer
 	}
 
 	// normalize each member of an array (doesn't add metadata for top level)
-	public function normalizeEach($data) {
-		return array_map(function ($item) { return $this->normalize($item); }, $data);
+	public function normalizeEach($data, $context = null, $limit = null) {
+		return array_map(function ($item) use ($context, $limit) {
+			return $this->normalize($item, $context, $limit);
+		}, $data);
 	}
 
 	public function trace(StackTrace $trace)
