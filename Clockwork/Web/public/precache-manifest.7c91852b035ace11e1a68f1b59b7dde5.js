69e11160990fdd52af039ce1f0f9284704e53b3c
its <its@luzer.sk>
2020-04-26 20:39:13 +0200

    Clockwork App 4.1.1.

--- Clockwork/Web/public/precache-manifest.7c91852b035ace11e1a68f1b59b7dde5.js
+++ Clockwork/Web/public/precache-manifest.7c91852b035ace11e1a68f1b59b7dde5.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "5e3a02c36b4e5e2a9e38",
-    "url": "css/app.af135e9c.css"
+    "revision": "8bcdbc45e6fb054dda7d",
+    "url": "css/app.74bdd08e.css"
   },
   {
-    "revision": "5e3a02c36b4e5e2a9e38",
-    "url": "js/app.648811c2.js"
+    "revision": "8bcdbc45e6fb054dda7d",
+    "url": "js/app.8b6f17a2.js"
   },
   {
     "revision": "9b438ea0b3dc7b4a5be1",
     "url": "js/chunk-vendors.b6cbf595.js"
   },
   {
-    "revision": "2fb83bc56cee691a7facbd3c80f3275f",
+    "revision": "bc5a253ab4e19b9f15b8dbf35938be8e",
     "url": "index.html"
+  },
+  {
+    "revision": "4a9a5b7dee3fc2c7d2a684e63c447b55",
+    "url": "img/appearance-dark-icon.png"
   },
   {
     "revision": "611a8f3ac0aa856f4018f676d719c67f",
@@ -24,20 +28,16 @@ self.__precacheManifest = [
     "url": "img/appearance-light-icon.png"
   },
   {
-    "revision": "4a9a5b7dee3fc2c7d2a684e63c447b55",
-    "url": "img/appearance-dark-icon.png"
-  },
-  {
     "revision": "cda1fba54a095fd43346d9f3fe06669a",
     "url": "img/whats-new/4.1/commands-profiling.png"
   },
   {
-    "revision": "03d36de2be5c98c325c6d7b115f81ace",
-    "url": "img/whats-new/4.1/extended-timeline.png"
-  },
-  {
     "revision": "bdb3d5c5df863ae277204f869eb12ed7",
     "url": "img/whats-new/4.1/queue-jobs-profiling.png"
+  },
+  {
+    "revision": "03d36de2be5c98c325c6d7b115f81ace",
+    "url": "img/whats-new/4.1/extended-timeline.png"
   },
   {
     "revision": "2ad52c54fbe33fd038d800d1f634bbaf",
