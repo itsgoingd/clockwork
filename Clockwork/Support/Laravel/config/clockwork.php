7526547bbb45872a54adf260aa2ea554d7872fb1
Edgard Lorraine Messias <edgardmessias@gmail.com>
2020-10-01 08:10:43 -0300

    chore: Ignore Laravel DebugBar requests by default

--- Clockwork/Support/Laravel/config/clockwork.php
+++ Clockwork/Support/Laravel/config/clockwork.php
@@ -173,6 +173,7 @@ return [
 		'except' => [
 			'/horizon/.*', // Laravel Horizon requests
 			'/telescope/.*', // Laravel Telescope requests
+			'/_debugbar/.*', // Laravel DebugBar requests
 		],
 
 		// List of URIs that should be collected, any other URI will not be collected if not empty
