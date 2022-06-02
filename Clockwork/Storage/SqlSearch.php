d60baa724553d4569cd89fae0f72f962744fd872
screw <screw@users.noreply.github.com>
2022-06-02 13:52:23 +0200

    Add quotes around field names in where clause

--- Clockwork/Storage/SqlSearch.php
+++ Clockwork/Storage/SqlSearch.php
@@ -71,10 +71,10 @@ class SqlSearch extends Search
 			return implode(' OR ', array_map(function ($input, $index) use ($field, &$bindings) {
 				if (preg_match('/^<(.+)$/', $input, $match)) {
 					$bindings["{$field}{$index}"] = $match[1];
-					return "{$field} < :{$field}{$index}";
+					return "\"{$field}\" < :{$field}{$index}";
 				} elseif (preg_match('/^>(.+)$/', $input, $match)) {
 					$bindings["{$field}{$index}"] = $match[1];
-					return "{$field} > :{$field}{$index}";
+					return "\"{$field}\" > :{$field}{$index}";
 				}
 			}, $inputs, array_keys($inputs)));
 		}, $fields));
@@ -95,7 +95,7 @@ class SqlSearch extends Search
 			}, $inputs, array_keys($inputs)));
 		}, $fields));
 
-		return [ "{$field} IN ({$values})", $bindings ];
+		return [ "\"{$field}\" IN ({$values})", $bindings ];
 	}
 
 	// Resolve a number type condition and bindings
@@ -108,17 +108,17 @@ class SqlSearch extends Search
 			return implode(' OR ', array_map(function ($input, $index) use ($field, &$bindings) {
 				if (preg_match('/^<(\d+(?:\.\d+)?)$/', $input, $match)) {
 					$bindings["{$field}{$index}"] = $match[1];
-					return "{$field} < :{$field}{$index}";
+					return "\"{$field}\" < :{$field}{$index}";
 				} elseif (preg_match('/^>(\d+(?:\.\d+)?)$/', $input, $match)) {
 					$bindings["{$field}{$index}"] = $match[1];
-					return "{$field} > :{$field}{$index}";
+					return "\"{$field}\" > :{$field}{$index}";
 				} elseif (preg_match('/^(\d+(?:\.\d+)?)-(\d+(?:\.\d+)?)$/', $input, $match)) {
 					$bindings["{$field}{$index}from"] = $match[1];
 					$bindings["{$field}{$index}to"] = $match[2];
-					return "({$field} > :{$field}{$index}from AND {$field} < :{$field}{$index}to)";
+					return "(\"{$field}\" > :{$field}{$index}from AND \"{$field}\" < :{$field}{$index}to)";
 				} else {
 					$bindings["{$field}{$index}"] = $input;
-					return "{$field} = :{$field}{$index}";
+					return "\"{$field}\" = :{$field}{$index}";
 				}
 			}, $inputs, array_keys($inputs)));
 		}, $fields));
@@ -135,7 +135,7 @@ class SqlSearch extends Search
 		$conditions = implode(' OR ', array_map(function ($field) use ($inputs, &$bindings) {
 			return implode(' OR ', array_map(function ($input, $index) use ($field, &$bindings) {
 				$bindings["{$field}{$index}"] = $input;
-				return "{$field} LIKE :{$field}{$index}";
+				return "\"{$field}\" LIKE :{$field}{$index}";
 			}, $inputs, array_keys($inputs)));
 		}, $fields));
 
