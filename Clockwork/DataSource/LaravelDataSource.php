9dc2685f1cfcd6d4dc3f8b6756cf9d3afed11337
its <its@luzer.sk>
2022-10-24 20:02:31 +0200

    Laravel - improved compatibility when resolving authenticated user.

--- Clockwork/DataSource/LaravelDataSource.php
+++ Clockwork/DataSource/LaravelDataSource.php
@@ -201,15 +201,23 @@ class LaravelDataSource extends DataSource
 		if (! isset($this->app['auth'])) return;
 		if (! ($user = $this->app['auth']->user())) return;
 
-		// retrieve attributes in this awkward way to make sure we don't trigger exceptions with Eloquent strict mode on
-		$keyName = $user->getKeyName();
-		$user = $user->getAttributes();
+		if ($user instanceof \Illuminate\Database\Eloquent\Model) {
+			// retrieve attributes in this awkward way to make sure we don't trigger exceptions with Eloquent strict mode on
+			$keyName = method_exists($user, 'getAuthIdentifierName') ? $user->getAuthIdentifierName() : $user->getKeyName();
+			$user = $user->getAttributes();
 
-		if (! isset($user['email']) || ! isset($user[$keyName])) return;
+			$userId = isset($user[$keyName]) ? $user[$keyName] : null;
+			$userEmail = isset($user['email']) ? $user['email'] : $userId;
+			$userName = isset($user['name']) ? $user['name'] : null;
+		} else {
+			$userId = $user->getAuthIdentifier();
+			$userEmail = isset($user->email) ? $user->email : $userId;
+			$userName = isset($user->name) ? $user->name : null;
+		}
 
-		$request->setAuthenticatedUser($user['email'], $user[$keyName], [
-			'email' => $user['email'],
-			'name'  => isset($user['name']) ? $user['name'] : null
+		$request->setAuthenticatedUser($userEmail, $userId, [
+			'email' => $userEmail,
+			'name'  => $userName
 		]);
 	}
 }
