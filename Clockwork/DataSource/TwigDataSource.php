465033a41fd37d3ebde1859d717ceb7311498c85
Erick Tucto <erick@ericktucto.com>
2023-05-14 19:49:57 -0500

    Update TwigDataSource to using engine Twig

--- Clockwork/DataSource/TwigDataSource.php
+++ Clockwork/DataSource/TwigDataSource.php
@@ -3,9 +3,10 @@
 use Clockwork\Request\Request;
 use Clockwork\Support\Twig\ProfilerClockworkDumper;
 
-use Twig_Environment;
-use Twig_Extension_Profiler;
-use Twig_Profiler_Profile;
+use Twig\Environment;
+use Twig\Extension\ProfilerExtension;
+use Twig\Profiler\Profile;
+
 
 // Data source for Twig, provides rendered views
 class TwigDataSource extends DataSource
@@ -17,7 +18,7 @@ class TwigDataSource extends DataSource
 	protected $profile;
 
 	// Create a new data source, takes Twig instance as an argument
-	public function __construct(Twig_Environment $twig)
+	public function __construct(Environment $twig)
 	{
 		$this->twig = $twig;
 	}
@@ -25,7 +26,9 @@ class TwigDataSource extends DataSource
 	// Register the Twig profiler extension
 	public function listenToEvents()
 	{
-		$this->twig->addExtension(new Twig_Extension_Profiler($this->profile = new Twig_Profiler_Profile));
+		$this->twig->addExtension(
+			new ProfilerExtension(($this->profile = new Profile()))
+		);
 	}
 
 	// Adds rendered views to the request
