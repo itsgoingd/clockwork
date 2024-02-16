77e31865a95f9800c99894a48e0d6b61888f551b
DominicDetta <detta.dominic87@gmail.com>
2024-02-16 14:27:15 +0100

    fix(#676): allow monolog datasource to set the correct handler by checking the return type of the write method

--- Clockwork/DataSource/MonologDataSource.php
+++ Clockwork/DataSource/MonologDataSource.php
@@ -3,9 +3,11 @@
 use Clockwork\DataSource\DataSource;
 use Clockwork\Request\Log;
 use Clockwork\Request\Request;
-use Clockwork\Support\Monolog\Handler\ClockworkHandler;
-
+use Clockwork\Support\Monolog\Monolog2\ClockworkHandler;
+use Clockwork\Support\Monolog\Monolog\ClockworkHandler as MonologClockworkHandler;
+use Monolog\Handler\AbstractProcessingHandler;
 use Monolog\Logger as Monolog;
+use ReflectionMethod;
 
 // Data source for Monolog, provides application log
 class MonologDataSource extends DataSource
@@ -18,7 +20,17 @@ class MonologDataSource extends DataSource
 	{
 		$this->log = new Log;
 
-		$monolog->pushHandler(new ClockworkHandler($this->log));
+		/**
+		 * Use `Clockwork\Support\Monolog\Monolog2\ClockworkHandler`
+		 * when the method `AbstractProcessingHandler::write` has a return type
+		 * Because Monolog 2 introduced scalar type hints and return hints
+		 */
+		$writeMethod = new ReflectionMethod(AbstractProcessingHandler::class, 'write');
+		if ($writeMethod->hasReturnType()) {
+			$monolog->pushHandler(new ClockworkHandler($this->log));
+		} else {
+			$monolog->pushHandler(new MonologClockworkHandler($this->log));
+		}
 	}
 
 	// Adds log entries to the request
