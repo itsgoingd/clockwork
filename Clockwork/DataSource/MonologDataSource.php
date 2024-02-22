f0a0d218b6e844fe971358d74bdb405ef97cdb7d
DominicDetta <detta.dominic87@gmail.com>
2024-02-22 16:03:50 +0100

    fix(#676): set monolog handler by checking the constant Monolog\Logger::API

--- Clockwork/DataSource/MonologDataSource.php
+++ Clockwork/DataSource/MonologDataSource.php
@@ -3,11 +3,9 @@
 use Clockwork\DataSource\DataSource;
 use Clockwork\Request\Log;
 use Clockwork\Request\Request;
-use Clockwork\Support\Monolog\Monolog2\ClockworkHandler;
-use Clockwork\Support\Monolog\Monolog\ClockworkHandler as MonologClockworkHandler;
-use Monolog\Handler\AbstractProcessingHandler;
+use Clockwork\Support\Monolog\Monolog2\ClockworkHandler as Monolog2ClockworkHandler;
+use Clockwork\Support\Monolog\Monolog\ClockworkHandler;
 use Monolog\Logger as Monolog;
-use ReflectionMethod;
 
 // Data source for Monolog, provides application log
 class MonologDataSource extends DataSource
@@ -20,17 +18,21 @@ class MonologDataSource extends DataSource
 	{
 		$this->log = new Log;
 
-		/**
-		 * Use `Clockwork\Support\Monolog\Monolog2\ClockworkHandler`
-		 * when the method `AbstractProcessingHandler::write` has a return type
-		 * Because Monolog 2 introduced scalar type hints and return hints
-		 */
-		$writeMethod = new ReflectionMethod(AbstractProcessingHandler::class, 'write');
-		if ($writeMethod->hasReturnType()) {
-			$monolog->pushHandler(new ClockworkHandler($this->log));
-		} else {
-			$monolog->pushHandler(new MonologClockworkHandler($this->log));
+		$handler=null;
+		switch (\Monolog\Logger::API) {
+			case 1:
+				$handler=new ClockworkHandler($this->log);
+				break;
+			case 2:
+				$handler=new Monolog2ClockworkHandler($this->log);
+				break;
+			default:
+				// By default use the latest implementation of clockwork handler
+				$handler=new Monolog2ClockworkHandler($this->log);
+				break;
 		}
+		
+		$monolog->pushHandler($handler);
 	}
 
 	// Adds log entries to the request
