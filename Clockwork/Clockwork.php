356b2017decc88d42fc08e903cb41277e3966da1
its <its@luzer.sk>
2024-03-27 22:22:54 +0100

    Bumped version to 5.2.1, updated changelog.

--- Clockwork/Clockwork.php
+++ Clockwork/Clockwork.php
@@ -16,7 +16,7 @@ use Closure;
 class Clockwork
 {
 	// Clockwork library version
-	const VERSION = '5.2';
+	const VERSION = '5.2.1';
 
 	// Array of data sources, these objects collect metadata for the current application run
 	protected $dataSources = [];
